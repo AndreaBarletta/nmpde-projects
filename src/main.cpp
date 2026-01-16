eb0ecf2179c909aaf4b1f516e978bea1d2f04e6e
enti <enrico.tirri@mail.polimi.it>
2026-01-16 16:56:34 +0100

    better error message for usage

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,15 @@
 // Main function.
 int main(int argc, char *argv[])
 {
+    if (argc != 4)
+    {
+        std::cerr << "Usage: ./shallow_waters T deltat mesh_file_name" << std::endl;
+        return 1;
+    }
+
     Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
 
     // Read command line arguments (T, deltat, mesh file name)
-    AssertThrow(argc == 4, ExcMessage("Usage: ./shallow_waters T deltat mesh_file_name"));
-
     const unsigned int degree_velocity = 2;
     const unsigned int degree_height = 1;
     const double T = std::stod(argv[1]);
